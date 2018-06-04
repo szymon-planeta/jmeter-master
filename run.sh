59ac97780c41ff3f4556a437a10369f8650c9a08
Szymon Planeta <planetaszymon@gmail.com>
2018-06-04 18:54:00 +0000

    update tests

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ mkdir -p reports
 
 jmx="${1}"
 
-JVM_ARGS="-Xms2g -Xmx2g" ./apache-jmeter-4.0/bin/jmeter -n -t "${jmx}" -l "${jmx}.log" -e -o reports
+JVM_ARGS="-Xms3g -Xmx3g" ./apache-jmeter-4.0/bin/jmeter -n -t "${jmx}" -l "reports/${jmx}.log" -e -o reports
